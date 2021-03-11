1ab6b3dfd2e1982601ecf44669b803b6f373231d
uf-code <ugur.firat.developer@gmail.com>
2021-03-11 09:38:02 +0300

    testing template refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="home">Home</div>
-  <p>My name is {{ name }} and my age is {{ age }}</p>
+  <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
   <button @click="handleClick">Click!</button>
 </template>
 
 <script>
+import { ref } from 'vue'
 // @ is an alias to /src
 
 export default {
   name: 'Home',
   components: {},
   setup() {
-    console.log('setup')
+    console.log(this)
+
+    const p = ref(null)
+    console.log(p)
 
     let name = 'mario'
     let age = 30
 
     const handleClick = () => {
-      console.log('Cliked!!!')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello, ninjas'
     }
 
-    return { name, age, handleClick }
+    return { name, age, handleClick, p }
   },
 }
 </script>
