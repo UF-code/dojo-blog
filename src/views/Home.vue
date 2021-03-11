a67f9ea0743557bb3c964a64edc4013deb1d08e4
uf-code <ugur.firat.developer@gmail.com>
2021-03-11 09:29:19 +0300

    composition api testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,8 @@
-<template> </template>
+<template>
+  <div class="home">Home</div>
+  <p>My name is {{ name }} and my age is {{ age }}</p>
+  <button @click="handleClick">Click!</button>
+</template>
 
 <script>
 // @ is an alias to /src
@@ -6,5 +10,17 @@
 export default {
   name: 'Home',
   components: {},
+  setup() {
+    console.log('setup')
+
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('Cliked!!!')
+    }
+
+    return { name, age, handleClick }
+  },
 }
 </script>
