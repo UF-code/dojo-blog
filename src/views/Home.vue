2d65081939245d8b8dbcda477871d712f95b9d52
uf-code <ugur.firat.developer@gmail.com>
2021-03-13 09:23:09 +0300

    computed properties with composition api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,37 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} = {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update Ninja One</button>
-
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} = {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update Ninja Two</button>
+    <input type="text" v-model="search" />
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, computed } from 'vue'
 // @ is an alias to /src
 
 export default {
   name: 'Home',
-  components: {},
+
   setup() {
-    const ninjaOne = ref({ name: 'Mario', age: 30 })
-    const ninjaTwo = reactive({ name: 'Luigi', age: 35 })
+    const search = ref('')
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
-    }
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45
-    }
+    const names = ref([
+      'mario',
+      'yoshi',
+      'luigi',
+      'toad',
+      'bowser',
+      'koopa',
+      'peach',
+    ])
 
-    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo }
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+    return { names, search, matchingNames }
   },
 }
 </script>
