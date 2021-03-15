b7ba3e922b47b6dcf386ca862ec6742764eeab69
uf-code <ugur.firat.developer@gmail.com>
2021-03-15 09:53:49 +0300

    fetching data in setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts">toggle post</button>
-    <button @click="posts.pop()">delete a post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading....</div>
   </div>
 </template>
 
@@ -15,23 +17,26 @@ export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: 'wellcome to the blog',
-        body:
-          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum nostrum fugiat non officiis commodi hic? Pariatur adipisci culpa quibusdam nostrum, blanditiis a quas accusantium deleniti, vel facilis ipsum nemo modi laboriosam cupiditate impedit qui temporibus odit ipsam debitis nisi? Natus perspiciatis ad molestiae optio nihil modi cupiditate, quam perferendis ipsum consequuntur est amet neque officiis, maiores omnis facilis. Fuga facilis nulla fugit unde consectetur recusandae repellat beatae voluptas dignissimos aspernatur sint nisi reprehenderit pariatur mollitia repudiandae neque cumque, ullam harum. Laboriosam, quod ipsum quam natus recusandae vero nihil enim ab, laudantium quasi illo et explicabo non amet? Placeat, et illo!',
-        id: 1,
-      },
-      {
-        title: 'top 5 CSS tips',
-        body:
-          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum nostrum fugiat non officiis commodi hic? Pariatur adipisci culpa quibusdam nostrum, blanditiis a quas accusantium deleniti, vel facilis ipsum nemo modi laboriosam cupiditate impedit qui temporibus odit ipsam debitis nisi? Natus perspiciatis ad molestiae optio nihil modi cupiditate, quam perferendis ipsum consequuntur est amet neque officiis, maiores omnis facilis. Fuga facilis nulla fugit unde consectetur recusandae repellat beatae voluptas dignissimos aspernatur sint nisi reprehenderit pariatur mollitia repudiandae neque cumque, ullam harum. Laboriosam, quod ipsum quam natus recusandae vero nihil enim ab, laudantium quasi illo et explicabo non amet? Placeat, et illo!',
-        id: 2,
-      },
-    ])
-    const showPosts = ref(true)
+    const posts = ref([])
+    const error = ref(null)
 
-    return { posts, showPosts }
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('no data available')
+        }
+
+        posts.value = await data.json()
+      } catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+
+    load()
+
+    return { posts, error }
   },
 }
 </script>
