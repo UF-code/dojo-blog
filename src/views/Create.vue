2de2fbf817e6f864229e572484919f38f1602729
uf-code <ugur.firat.developer@gmail.com>
2021-03-18 01:38:31 +0300

    web form created

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,7 +1,38 @@
-<template> </template>
+<template>
+  <div class="create">
+    <form>
+      <label>Title: </label>
+      <input v-model="title" type="text" required />
+      <label>Content: </label>
+      <textarea v-model="body" required></textarea>
+      <label>Tags (hit enter to add a tag)</label>
+      <input v-model="tag" type="text" @keydown.enter.prevent="handleKeydown" />
+      <button>Add Post</button>
+    </form>
+  </div>
+</template>
 
 <script>
-export default {}
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const title = ref('')
+    const body = ref('')
+    const tag = ref('')
+    const tags = ref([])
+
+    const handleKeydown = () => {
+      if (!tags.value.includes(tag.value)) {
+        tag.value = tag.value.replace(/\s/, '') // removes all whitespace
+        tags.value.push(tag.value)
+      }
+      tag.value = ''
+    }
+
+    return { title, body, tag, tags }
+  },
+}
 </script>
 
 <style></style>
