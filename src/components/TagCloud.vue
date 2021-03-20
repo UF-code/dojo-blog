31bbde2c4dda006acc32b07d1b5ed74e904e55db
uf-code <ugur.firat.developer@gmail.com>
2021-03-20 08:26:34 +0300

    useTags composable function is created

---
+++ src/components/TagCloud.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tag-cloud">
+    <h3>Tags</h3>
+    <div></div>
+  </div>
+</template>
+
+<script>
+import useTags from '../composables/useTags'
+
+export default {
+  props: ['posts'],
+  setup() {
+    const { tags } = useTags(props.posts)
+  },
+}
+</script>
+
+<style></style>
